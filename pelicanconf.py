cd032cbe296f470c3393f2e58ef1e357458a3187
Diego Becker <diegobecker00@gmail.com>
2017-01-05 16:55:19 -0200

    Adicionando tema Flex e arquivo CNAME

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,31 +3,50 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Diego Becker'
-SITENAME = 'Diego Becker'
-SITEURL = ''
+SITEURL = 'http://localhost:8000'
+SITENAME = "Diego Becker's Blog"
+SITETITLE = AUTHOR
+SITESUBTITLE = 'Sotfware Developer'
+SITELOGO = '//s.gravatar.com/avatar/27e62c171c9f88dbb0d57e534d2e52a4?s=120'
 
+BROWSER_COLOR = '#333333'
+PYGMENTS_STYLE = 'monokai'
+
+THEME = '../tema-flex'
 PATH = 'content'
-
 TIMEZONE = 'America/Sao_Paulo'
-
 DEFAULT_LANG = 'pt'
 
+
+
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+USE_FOLDER_AS_CATEGORY = False
+MAIN_MENU = True
+
+STATIC_PATHS = ['extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
+
+
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('linkedin', 'https://br.linkedin.com/in/diegoobecker'),
+          ('github', 'https://github.com/diegobecker'),
+          #('google', 'https://google.com/+AlexandreVicenzi'),
+          ('twitter', 'https://twitter.com/_diegobecker_')
+          #('rss', '//blog.alexandrevicenzi.com/feeds/all.atom.xml')
+          )
+DISQUS_SITENAME = 'diegobecker'
 
 DEFAULT_PAGINATION = 10
 
